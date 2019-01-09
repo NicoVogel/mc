a54298dfe8b6fa38d458031a395d46f0090852b4
BirdIT <nico-vogel97@outlook.de>
2019-01-09 17:28:24 +0100

    update

--- MinecraftClone/src/main/java/TestRun.java
+++ MinecraftClone/src/main/java/TestRun.java
@@ -5,6 +5,7 @@ public class TestRun {
 
 	public static void main(String[] args) {
 		JavaEngine engine = EngineProvider.getEngine();
+		engine.getInputs().addKeyListener(e -> System.out.println("hallo " + e));
 		engine.showWindow();
 		while (!engine.isClosed()) {
 			engine.update();
