7dda8bf857c0b0399df1557b0c83425548a4f829
BirdIT <nico-vogel97@outlook.de>
2019-01-09 16:40:52 +0100

    add

---
+++ MinecraftClone/src/main/java/dhbw/engine/impl/input/KeyInputListener.java
@@ -0,0 +1,8 @@
+package dhbw.engine.impl.input;
+
+@FunctionalInterface
+public interface KeyInputListener {
+
+	void keyInput(KeyEvent e);
+
+}
---
+++ MinecraftClone/src/main/java/dhbw/engine/impl/input/MouseInputListener.java
@@ -0,0 +1,7 @@
+package dhbw.engine.impl.input;
+
+@FunctionalInterface
+public interface MouseInputListener {
+
+	void mouseInput(MouseEvent e);
+}
