45e1a34f6f4876e02c57e8ca77c174cfaa8b30d9
BirdIT <nico-vogel97@outlook.de>
2019-01-09 16:39:59 +0100

    add

---
+++ MinecraftClone/src/main/java/dhbw/engine/impl/input/MouseEvent.java
@@ -0,0 +1,28 @@
+package dhbw.engine.impl.input;
+
+import java.util.EventObject;
+
+import lombok.Getter;
+
+@Getter
+public class MouseEvent extends EventObject {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8191581388078557863L;
+
+	private int mouseCode;
+	private boolean mouseDown;
+	private boolean mousePressed;
+	private boolean mouseRelease;
+
+	public MouseEvent(Object source, int mouseCode, boolean mouseDown, boolean mousePressed, boolean mouseRelease) {
+		super(source);
+		this.mouseCode = mouseCode;
+		this.mouseDown = mouseDown;
+		this.mousePressed = mousePressed;
+		this.mouseRelease = mouseRelease;
+	}
+
+}
