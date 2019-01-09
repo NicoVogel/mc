5391761904ff8bc599e5612eca0b44cd79d26726
BirdIT <nico-vogel97@outlook.de>
2019-01-09 15:37:56 +0100

    add

---
+++ MinecraftClone/src/main/java/TestRun.java
@@ -0,0 +1,12 @@
+import dhbw.engine.EngineProvider;
+import dhbw.engine.JavaEngine;
+
+public class TestRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JavaEngine engine = EngineProvider.getEngine();
+		engine.showWindow();
+	}
+
+}
