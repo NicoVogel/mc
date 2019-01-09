e19a69b8386a3b9c0091979393311c2e59ed0d23
BirdIT <nico-vogel97@outlook.de>
2019-01-09 13:22:26 +0100

    create main class

---
+++ MinecraftClone/src/main/java/dhbw/engine/JavaEngine.java
@@ -0,0 +1,15 @@
+package dhbw.engine;
+
+public class JavaEngine {
+
+	private JavaEngine() {
+
+	}
+
+	private static JavaEngine engine;
+
+	public static JavaEngine getEngine() {
+		return engine;
+	}
+
+}
