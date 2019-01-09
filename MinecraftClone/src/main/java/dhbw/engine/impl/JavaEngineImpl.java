49940e0f4d4b017e46efc156adfc0a1930c35822
BirdIT <nico-vogel97@outlook.de>
2019-01-09 14:16:35 +0100

    remove singelton

--- MinecraftClone/src/main/java/dhbw/engine/impl/JavaEngineImpl.java
+++ MinecraftClone/src/main/java/dhbw/engine/impl/JavaEngineImpl.java
@@ -4,14 +4,4 @@ import dhbw.engine.JavaEngine;
 
 public class JavaEngineImpl implements JavaEngine {
 
-	private JavaEngineImpl() {
-
-	}
-
-	private static JavaEngineImpl engine;
-
-	public static JavaEngine getEngine() {
-		return engine;
-	}
-
 }
