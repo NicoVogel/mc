858155472a903a3777f51eb54db36694cda6af97
BirdIT <nico-vogel97@outlook.de>
2019-01-09 14:59:35 +0100

    add

---
+++ MinecraftClone/src/main/java/dhbw/engine/impl/display/DisplayInitialSettingsBE.java
@@ -0,0 +1,12 @@
+package dhbw.engine.impl.display;
+
+import lombok.Data;
+
+@Data
+public class DisplayInitialSettingsBE {
+
+	private int width = 800;
+	private int height = 600;
+	private String title = "test";
+
+}
---
+++ MinecraftClone/src/main/java/dhbw/engine/impl/display/DisplayManager.java
@@ -0,0 +1,9 @@
+package dhbw.engine.impl.display;
+
+public class DisplayManager {
+
+	public void createDisplay() {
+
+	}
+
+}
