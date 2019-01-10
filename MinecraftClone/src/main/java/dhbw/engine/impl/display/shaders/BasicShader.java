52f7f10789abef7ba5e20f390dc95731a04c9986
BirdIT <nico-vogel97@outlook.de>
2019-01-10 13:50:22 +0100

    simple implementation of shader

--- MinecraftClone/src/main/java/dhbw/engine/impl/display/shaders/BasicShader.java
+++ MinecraftClone/src/main/java/dhbw/engine/impl/display/shaders/BasicShader.java
@@ -2,8 +2,8 @@ package dhbw.engine.impl.display.shaders;
 
 public class BasicShader extends Shader {
 
-	private static final String VERTEX_FILE = "";
-	private static final String FRAGMENT_FILE = "";
+	private static final String VERTEX_FILE = "./src/main/resources/basicVertexShader.glsl";
+	private static final String FRAGMENT_FILE = "./src/main/resources/basicFragmentShader.glsl";
 
 	public BasicShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
