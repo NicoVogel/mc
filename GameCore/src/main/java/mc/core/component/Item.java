ed936a4c9ae82eb2b1b88c0deb0dcb40e7855306
Nico Vogel <nico-vogel97@outlook.de>
2019-02-21 20:07:23 +0100

    update

--- GameCore/src/main/java/mc/core/component/Item.java
+++ GameCore/src/main/java/mc/core/component/Item.java
@@ -1,10 +1,20 @@
 package mc.core.component;
 
+import lombok.Getter;
+import lombok.Setter;
 import mc.core.Component;
 import mc.core.ComponentCollection;
 
+@Getter
 public class Item extends Component {
 
+	private int id;
+	@Setter
+	private String name;
+	@Setter
+	private String description;
+	
+	
 	public Item(ComponentCollection parent) {
 		super(parent);
 		// TODO Auto-generated constructor stub
