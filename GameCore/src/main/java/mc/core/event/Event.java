39c6569b6a969d6b5ec7bbdb7345dea9f657acb5
BirdIT <nico-vogel97@outlook.de>
2019-02-21 01:31:33 +0100

    update

--- GameCore/src/main/java/mc/core/event/Event.java
+++ GameCore/src/main/java/mc/core/event/Event.java
@@ -1,13 +1,44 @@
 package mc.core.event;
 
-import lombok.AllArgsConstructor;
+import java.util.HashSet;
 
-@AllArgsConstructor
 public class Event<T> {
 
-	private EventProvider<T> provider;
+	private HashSet<EventListener<T>> listeners;
 
+	private HashSet<EventListener<T>> getListeners() {
+		if (this.listeners == null) {
+			this.listeners = new HashSet<>();
+		}
+		return this.listeners;
+	}
+
+	/**
+	 * add an listener
+	 * 
+	 * @param listener
+	 */
+	public void add(EventListener<T> listener) {
+		getListeners().add(listener);
+	}
+
+	/**
+	 * remove an listener
+	 * 
+	 * @param listener
+	 */
+	public void remove(EventListener<T> listener) {
+		getListeners().remove(listener);
+	}
+
+	/**
+	 * fire event
+	 * 
+	 * @param object
+	 */
 	public void invoke(T object) {
-		this.provider.invoke(object);
+		for (EventListener<T> eventInvoke : listeners) {
+			eventInvoke.invoke(object);
+		}
 	}
 }
--- GameCore/src/main/java/mc/core/event/EventProvider.java
+++ GameCore/src/main/java/mc/core/event/EventProvider.java
@@ -1,45 +1,18 @@
 package mc.core.event;
 
-import java.util.HashSet;
-
-public class EventProvider<T> {
-
-	private HashSet<EventListener<T>> listeners;
-
-	private HashSet<EventListener<T>> getListeners() {
-		if (this.listeners == null) {
-			this.listeners = new HashSet<>();
-		}
-		return this.listeners;
-	}
+public interface EventProvider<T> {
 
 	/**
 	 * add an listener
 	 * 
 	 * @param listener
 	 */
-	public void add(EventListener<T> listener) {
-		getListeners().add(listener);
-	}
+	public void add(EventListener<T> listener);
 
 	/**
 	 * remove an listener
 	 * 
 	 * @param listener
 	 */
-	public void remove(EventListener<T> listener) {
-		getListeners().remove(listener);
-	}
-
-	/**
-	 * invoke the event
-	 * 
-	 * @param object
-	 */
-	/* package */ void invoke(T object) {
-		for (EventListener<T> eventInvoke : listeners) {
-			eventInvoke.invoke(object);
-		}
-	}
-
+	public void remove(EventListener<T> listener);
 }
