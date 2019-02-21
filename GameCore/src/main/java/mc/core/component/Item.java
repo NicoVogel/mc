bd36755f1612585db739bcc0c62a52294712f804
Nico Vogel <nico-vogel97@outlook.de>
2019-02-21 15:09:12 +0100

    add

---
+++ GameCore/src/main/java/mc/core/component/Item.java
@@ -0,0 +1,13 @@
+package mc.core.component;
+
+import mc.core.Component;
+import mc.core.ComponentCollection;
+
+public class Item extends Component {
+
+	public Item(ComponentCollection parent) {
+		super(parent);
+		// TODO Auto-generated constructor stub
+	}
+
+}
