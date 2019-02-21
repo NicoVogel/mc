1101d23cb3ef92a2d229e38d5a723fac63efb6cb
BirdIT <nico-vogel97@outlook.de>
2019-02-21 01:07:56 +0100

    update

--- GameCore/src/main/java/mc/core/component/Block.java
+++ GameCore/src/main/java/mc/core/component/Block.java
@@ -1,7 +1,32 @@
 package mc.core.component;
 
-import mc.core.GameObject;
+import lombok.Getter;
+import mc.core.Component;
+import mc.core.ComponentCollection;
 
-public class Block extends GameObject {
+@Getter
+public class Block extends Component {
+
+	private Texture texture;
+	private WorldPosition position;
+
+	public Block(ComponentCollection parent, int x, int y, int z, String texture) {
+		super(parent);
+		if (parent == null) {
+			// TODO log
+			return;
+		}
+		this.position = new WorldPosition(parent, x, y, z);
+		this.texture = new Texture(parent, texture);
+	}
+
+	@Override
+	public void close() {
+		if (this.isClosed())
+			return;
+		super.close();
+		this.texture.close();
+		this.position.close();
+	}
 
 }
