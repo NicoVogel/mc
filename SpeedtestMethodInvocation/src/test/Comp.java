2b64b63b12edf6e57b47041e0b4bf83d586491e2
BirdIT <nico-vogel97@outlook.de>
2019-02-22 01:42:11 +0100

    add speedtest

---
+++ SpeedtestMethodInvocation/src/test/Comp.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Comp {
+	public void foo() {
+		for (int i = 0; i < 100000; i++) {
+			new Object();
+		}
+	}
+}
---
+++ SpeedtestMethodInvocation/src/test/Main.java
@@ -0,0 +1,50 @@
+package test;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+	static String className = "test.Comp";
+	static String methodName = "foo";
+	static int amount = 20;
+
+	public static void main(String[] args) throws Exception {
+
+		ArrayList<Long> testNormal = new ArrayList<>();
+		ArrayList<Long> testInvoke = new ArrayList<>();
+
+		Comp comp = new Comp();
+
+		Class<?> c = Class.forName(className);
+		Object obj = c.newInstance();
+		Method method = c.getDeclaredMethod(methodName);
+
+		System.out.println("start measurement normal");
+		measure(() -> comp.foo(), testNormal);
+		System.out.println(
+				"end measurement normal, avg: " + testNormal.stream().collect(Collectors.averagingLong(x -> x)));
+
+		System.out.println("start measurement invoke");
+		measure(() -> method.invoke(obj), testInvoke);
+		System.out.println(
+				"end measurement invoke, avg: " + testInvoke.stream().collect(Collectors.averagingLong(x -> x)));
+
+	}
+
+	private static void measure(inv method, List<Long> results) throws Exception {
+		for (int i = 0; i < amount; i++) {
+			long start = System.nanoTime();
+			method.invoke();
+			long end = System.nanoTime();
+			results.add(end - start);
+		}
+	}
+
+	@FunctionalInterface
+	interface inv {
+		void invoke() throws Exception;
+	}
+
+}
