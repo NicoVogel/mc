9a76d1f5c145e8b4fc3b27ea1749c1f2e0294fd1
BirdIT <nico-vogel97@outlook.de>
2019-03-10 23:24:28 +0100

    add pressedSince atribute

--- GameCore/src/main/java/mc/core/engine/model/InputEvent.java
+++ GameCore/src/main/java/mc/core/engine/model/InputEvent.java
@@ -1,9 +1,12 @@
-package mc.core.engine.communication;
+package mc.core.engine.model;
 
 import lombok.Getter;
 
 @Getter
 public class InputEvent {
+
+	// in miliseconds
+	private int pressedSince;
 
 	private char key;
 	private boolean keyDown;
@@ -19,7 +22,7 @@ public class InputEvent {
 	private double mousePosY;
 
 	public InputEvent(int mouseCode, boolean mouseDown, boolean mousePressed, boolean mouseRelease, double mousePosX,
-			double mousePosY) {
+			double mousePosY, int pressedSince) {
 		this.mouseCode = mouseCode;
 		this.mouseDown = mouseDown;
 		this.mousePressed = mousePressed;
@@ -28,7 +31,7 @@ public class InputEvent {
 		this.mousePosY = mousePosY;
 	}
 
-	public InputEvent(char key, boolean keyDown, boolean keyPressed, boolean keyRelease) {
+	public InputEvent(char key, boolean keyDown, boolean keyPressed, boolean keyRelease, int pressedSince) {
 		this.key = key;
 		this.keyDown = keyDown;
 		this.keyPressed = keyPressed;
