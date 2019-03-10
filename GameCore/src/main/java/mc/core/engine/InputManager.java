5139dc47dad9e869eceb638938d5cc892c4d4dda
BirdIT <nico-vogel97@outlook.de>
2019-03-10 23:24:13 +0100

    update

--- GameCore/src/main/java/mc/core/engine/InputManager.java
+++ GameCore/src/main/java/mc/core/engine/InputManager.java
@@ -1,26 +1,33 @@
 package mc.core.engine;
 
 import mc.core.event.Event;
+import mc.core.event.EventProvider;
 
 public class InputManager {
 
 	private Engine engine;
 
-	private Event<KeyEvent> keyEventProvider;
-	private Event<MouseEvent> mouseEventProvider;
+	private Event<KeyEvent> keyEvent;
+	private Event<MouseEvent> mouseEvent;
 
 	public InputManager(Engine engine) {
 		this.engine = engine;
 		if (this.engine == null) {
 			throw new IllegalArgumentException("the display manager was not set to an instance");
 		}
-		this.keyEventProvider = new Event<>();
-		this.mouseEventProvider = new Event<>();
+		this.keyEvent = new Event<>();
+		this.mouseEvent = new Event<>();
 	}
 
 	public void update() {
 		// TODO check engine buffer
 	}
 
-	public EventProvider<KeyEvent> OnKeyEvent
+	public EventProvider<KeyEvent> OnKey() {
+		return this.keyEvent;
+	}
+
+	public EventProvider<MouseEvent> OnMouse() {
+		return this.mouseEvent;
+	}
 }
