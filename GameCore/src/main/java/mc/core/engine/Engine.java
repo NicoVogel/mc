066aee6d0da8ea5d5773a1c155463c9051d13584
BirdIT <nico-vogel97@outlook.de>
2019-03-10 23:43:18 +0100

    define interface

--- GameCore/src/main/java/mc/core/engine/Engine.java
+++ GameCore/src/main/java/mc/core/engine/Engine.java
@@ -1,5 +1,25 @@
 package mc.core.engine;
 
+import java.util.Queue;
+import java.util.stream.Stream;
+
+import mc.core.engine.model.InputEvent;
+import mc.core.world.Chunk;
+
 public interface Engine {
+
+	Queue<InputEvent> getEventQueue();
+
+	Stream<Chunk> getChuncs();
+
+	double getPlayerX();
+
+	double getPlayerY();
+
+	double getPlayerZ();
+
+	double getCameraPitch();
+
+	double getCameraYaw();
 
 }
