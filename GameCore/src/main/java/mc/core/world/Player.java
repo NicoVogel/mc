bfb7740c19c313f1ed995169e9e3fcf9f909eab2
BirdIT <nico-vogel97@outlook.de>
2019-03-11 14:32:25 +0100

    add

---
+++ GameCore/src/main/java/mc/core/world/Player.java
@@ -0,0 +1,17 @@
+package mc.core.world;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Player {
+
+	private double x;
+	private double y;
+	private double z;
+
+	private double pitch;
+	private double yaw;
+
+}
