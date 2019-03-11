8534c3308b4e3b89430bd1e92158776abe1b78c4
BirdIT <nico-vogel97@outlook.de>
2019-03-11 14:35:43 +0100

    add

---
+++ GameCore/src/main/java/mc/core/world/WorldObject.java
@@ -0,0 +1,16 @@
+package mc.core.world;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WorldObject {
+
+	private int id;
+	private int state;
+	private double x;
+	private double y;
+	private double z;
+
+}
