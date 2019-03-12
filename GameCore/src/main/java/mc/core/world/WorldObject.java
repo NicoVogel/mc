235d9ccbfe6728b345d7f39879e647456fd26670
Nico Vogel <nico-vogel97@outlook.de>
2019-03-12 21:09:28 +0100

    implement disposable

--- GameCore/src/main/java/mc/core/world/WorldObject.java
+++ GameCore/src/main/java/mc/core/world/WorldObject.java
@@ -2,27 +2,55 @@ package mc.core.world;
 
 import org.joml.Vector3d;
 
+import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
+import mc.core.event.Disposable;
+import mc.core.event.Event;
+import mc.core.event.EventProvider;
+import mc.core.world.event.WorldObjectEvent;
 
 @Getter
-@Setter
-public class WorldObject {
+public class WorldObject implements Disposable {
 
 	private int id;
 	private int state;
 	private double x;
 	private double y;
 	private double z;
+	private Chunk parent;
+	@Getter(value = AccessLevel.PRIVATE)
+	private Event<WorldObjectEvent> stateChanged;
+	
+	public WorldObject(int id, int state, Chunk chunk, Vector3d position) {
+		this(id, state, chunk, position.x, position.y, position.z);
+	}
+	
+	public WorldObject(int id, int state, Chunk chunk, double x, double y, double z) {
+		this.id = id;
+		this.state = state;
+		this.parent = chunk;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 	
 	public Vector3d getPosition() {
 		return new Vector3d(this.x, this.y, this.z);
 	}
 	
-	public void setPosition(Vector3d position) {
-		this.x = position.x;
-		this.y = position.y;
-		this.z = position.z;
+	public void setState(int state) {
+		if(this.state != state) {
+			
+		}
+	}
+	
+	public EventProvider<WorldObjectEvent> OnStateChanged(){
+		return this.stateChanged;
+	}
+
+	@Override
+	public void dispose() {
+		this.stateChanged.dispose();
 	}
 
 }
