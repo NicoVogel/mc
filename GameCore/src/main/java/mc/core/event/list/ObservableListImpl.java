cd8501cec29de897c0c21373b8f4cf82397a03e4
Nico Vogel <nico-vogel97@outlook.de>
2019-03-12 19:53:09 +0100

    implement observable elements and all list methods

--- GameCore/src/main/java/mc/core/event/list/ObservableListImpl.java
+++ GameCore/src/main/java/mc/core/event/list/ObservableListImpl.java
@@ -11,178 +11,201 @@ import mc.core.event.EventProvider;
 
 public class ObservableListImpl<E> implements ObservableList<E> {
 
-	private ArrayList<E> list = new ArrayList<>();
+	private List<E> list;
 
-	private Event<ListAddEvent> onAdd;
+	private Event<E> onAdd;
+	private Event<E> onRemove;
+	private Event<Collection<? extends E>> onAddAll;
+	private Event<Collection<? extends E>> onRemoveAll;
+	private Event<ListSetEvent<E>> onSet;
 	
+	public ObservableListImpl(List<E> list) {
+		this.list = list;
+		this.onAdd = new Event<>();
+		this.onRemove = new Event<>();
+		this.onAddAll = new Event<>();
+		this.onRemoveAll = new Event<>();
+		this.onSet = new Event<>();
+	}
 	
+	public ObservableListImpl() {
+		this(new ArrayList<>());
+	}
 
 	@Override
 	public boolean add(E arg0) {
-		// TODO Auto-generated method stub
+		if(this.list.add(arg0)) {
+			this.onAdd.invoke(this, arg0);
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public void add(int arg0, E arg1) {
-		// TODO Auto-generated method stub
-
+		this.list.add(arg0, arg1);
+		this.onAdd.invoke(this, arg1);
 	}
 
 	@Override
 	public boolean addAll(Collection<? extends E> arg0) {
-		// TODO Auto-generated method stub
+		if(this.addAll(arg0)) {
+			this.onAddAll.invoke(this, arg0);
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean addAll(int arg0, Collection<? extends E> arg1) {
-		// TODO Auto-generated method stub
+		if(this.list.addAll(arg0, arg1)) {
+			this.onAddAll.invoke(this, arg1);
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-
+		Collection<? extends E> elements = this.list;
+		this.list.clear();
+		if(elements.isEmpty() == false) {
+			this.onRemoveAll.invoke(this, elements);
+		}
 	}
 
 	@Override
 	public boolean contains(Object arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.list.contains(arg0);
 	}
 
 	@Override
 	public boolean containsAll(Collection<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.list.containsAll(arg0);
 	}
 
 	@Override
 	public E get(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.list.get(arg0);
 	}
 
 	@Override
 	public int indexOf(Object arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.list.indexOf(arg0);
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.list.isEmpty();
 	}
 
 	@Override
 	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.list.iterator();
 	}
 
 	@Override
 	public int lastIndexOf(Object arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.list.lastIndexOf(arg0);
 	}
 
 	@Override
 	public ListIterator<E> listIterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.list.listIterator();
 	}
 
 	@Override
 	public ListIterator<E> listIterator(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.list.listIterator(arg0);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public boolean remove(Object arg0) {
-		// TODO Auto-generated method stub
+		if(this.list.remove(arg0)) {
+			this.onRemove.invoke(this, (E)arg0);
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public E remove(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		E element = this.list.remove(arg0);
+		this.onRemove.invoke(this, element);
+		return element;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public boolean removeAll(Collection<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		ArrayList<E> removed = new ArrayList<>();
+		boolean hasChanged = false;
+		for(Object obj : arg0){
+			if(this.list.remove(obj)) {
+				removed.add((E)obj);
+				hasChanged = true;
+			}
+		}
+		this.onRemoveAll.invoke(this, removed);
+		return hasChanged;
 	}
 
 	@Override
 	public boolean retainAll(Collection<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.list.retainAll(arg0);
 	}
 
 	@Override
 	public E set(int arg0, E arg1) {
-		// TODO Auto-generated method stub
-		return null;
+		E value = this.list.set(arg0, arg1);
+		this.onSet.invoke(this, new ListSetEvent<E>(value, arg1));
+		return value;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.list.size();
 	}
 
 	@Override
 	public List<E> subList(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.list.subList(arg0, arg1);
 	}
 
 	@Override
 	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.list.toArray();
 	}
 
 	@Override
 	public <T> T[] toArray(T[] arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.list.toArray(arg0);
 	}
 
 	@Override
 	public EventProvider<E> OnAdd() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.onAdd;
 	}
 
 	@Override
 	public EventProvider<E> OnRemove() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.onRemove;
 	}
 
 	@Override
-	public EventProvider<E> OnAddCollection() {
-		// TODO Auto-generated method stub
-		return null;
+	public EventProvider<Collection<? extends E>> OnAddCollection() {
+		return this.onAddAll;
 	}
 
 	@Override
-	public EventProvider<E> OnRemoveCollection() {
-		// TODO Auto-generated method stub
-		return null;
+	public EventProvider<Collection<? extends E>> OnRemoveCollection() {
+		return this.onRemoveAll;
 	}
 
 	@Override
-	public EventProvider<E> OnSet() {
-		// TODO Auto-generated method stub
-		return null;
+	public EventProvider<ListSetEvent<E>> OnSet() {
+		return this.onSet;
 	}
 
 }
