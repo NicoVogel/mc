fabde0a8d848b93d5bc9c034a3baa266d2a09c72
Nico Vogel <nico-vogel97@outlook.de>
2019-03-12 19:05:01 +0100

    add camera change event

--- GameCore/src/main/java/mc/core/world/Player.java
+++ GameCore/src/main/java/mc/core/world/Player.java
@@ -17,22 +17,29 @@ public class Player {
 	private double yaw;
 
 	@Getter(value = AccessLevel.PRIVATE)
-	private Event<PlayerEvent> onUpdatePosition = new Event<>();
+	private Event<PlayerEvent> onUpdate = new Event<>();
+	
 
 	public void setPosition(double x, double y, double z) {
 		if (this.x != x || this.y != y || this.z != z) {
 			PlayerEvent event = new PlayerEvent(this.x, this.y, this.z, x, y, z);
-			this.onUpdatePosition.invoke(this, event);
+			this.onUpdate.invoke(this, event);
+			this.x = x;
+			this.y = y;
+			this.z = z;
 		}
 	}
 	
 	public void setCamera(double pitch, double yaw) {
 		if(this.pitch != pitch || this.yaw != yaw) {
-			
+			PlayerEvent event = new PlayerEvent(this.pitch, this.yaw, pitch, yaw);
+			this.onUpdate.invoke(this, event);
+			this.pitch = pitch;
+			this.yaw = yaw;
 		}
 	}
 
-	public EventProvider<PlayerEvent> OnUpdatePosition() {
-		return this.onUpdatePosition;
+	public EventProvider<PlayerEvent> OnUpdate() {
+		return this.onUpdate;
 	}
 }
