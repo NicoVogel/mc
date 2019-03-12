55053fe01b20789003fe21c694a82f71324bc1df
Nico Vogel <nico-vogel97@outlook.de>
2019-03-12 19:52:48 +0100

    update

--- GameCore/src/main/java/mc/core/event/list/ObservableList.java
+++ GameCore/src/main/java/mc/core/event/list/ObservableList.java
@@ -1,19 +1,40 @@
 package mc.core.event.list;
 
+import java.util.Collection;
 import java.util.List;
 
 import mc.core.event.EventProvider;
 
 public interface ObservableList<E> extends List<E> {
 
+	/**
+	 * get the added element
+	 * @return
+	 */
 	EventProvider<E> OnAdd();
 	
+	/**
+	 * get the removed element
+	 * @return
+	 */
 	EventProvider<E> OnRemove();
 	
-	EventProvider<E> OnAddCollection();
+	/**
+	 * get all added elements
+	 * @return
+	 */
+	EventProvider<Collection<? extends E>> OnAddCollection();
 	
-	EventProvider<E> OnRemoveCollection();
+	/**
+	 * get all removed elements
+	 * @return
+	 */
+	EventProvider<Collection<? extends E>> OnRemoveCollection();
 	
-	EventProvider<E> OnSet();
+	/**
+	 * get the old and new element
+	 * @return
+	 */
+	EventProvider<ListSetEvent<E>> OnSet();
 	
 }
