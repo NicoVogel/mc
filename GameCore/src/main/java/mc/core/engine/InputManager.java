0c0654c4951decd26b3e0e240d1d4ccf6620b26e
Nico Vogel <nico-vogel97@outlook.de>
2019-03-12 17:43:10 +0100

    implement update method

--- GameCore/src/main/java/mc/core/engine/InputManager.java
+++ GameCore/src/main/java/mc/core/engine/InputManager.java
@@ -1,5 +1,11 @@
 package mc.core.engine;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+
+import mc.core.engine.model.InputEvent;
 import mc.core.event.Event;
 import mc.core.event.EventProvider;
 import mc.core.update.Updateable;
@@ -14,7 +20,7 @@ public class InputManager implements Updateable{
 	public InputManager(Engine engine) {
 		this.engine = engine;
 		if (this.engine == null) {
-			throw new IllegalArgumentException("the display manager was not set to an instance");
+			throw new IllegalArgumentException("cannot create an input manager without a engine");
 		}
 		this.keyEvent = new Event<>();
 		this.mouseEvent = new Event<>();
@@ -22,7 +28,39 @@ public class InputManager implements Updateable{
 
 	@Override
 	public void update() {
-		// TODO check engine buffer
+		Queue<InputEvent> inputQueue = this.engine.getInputQueue();
+		HashSet<Character> keyInput = new HashSet<>();
+		HashSet<Integer> mouseInput = new HashSet<>();
+		List<KeyEvent> keyEvent = new ArrayList<>();
+		List<MouseEvent> mouseEvent = new ArrayList<>();
+		
+		// for all events in queue
+		while(inputQueue.isEmpty() == false) {
+			InputEvent event = inputQueue.poll();
+			if(event.isKeyEvent()) {
+				
+				// get input as key event and add it if it does not already exist
+				KeyEvent key = event.newKeyEvent(this.engine);
+				if(keyInput.add(key.getKey())) {
+					keyEvent.add(key);
+				}
+			}else {
+				
+				// get input as mouse event and add if it does not already exist
+				MouseEvent mouse = event.newMouseEvent(this.engine);
+				if(mouseInput.add(mouse.getMouseCode())) {
+					mouseEvent.add(mouse);
+				}
+			}
+		}
+		
+		// invoke all event listener
+		for (MouseEvent m : mouseEvent) {
+			this.mouseEvent.invoke(this.engine, m);
+		}
+		for (KeyEvent k : keyEvent) {
+			this.keyEvent.invoke(this.engine, k);
+		}
 	}
 
 	public EventProvider<KeyEvent> OnKey() {
