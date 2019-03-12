0a1497f8ff09afb1a8606a0ff5aa9b14aa62460f
BirdIT <nico-vogel97@outlook.de>
2019-03-12 21:42:52 +0100

    update

--- GameCore/src/main/java/mc/core/engine/Engine.java
+++ GameCore/src/main/java/mc/core/engine/Engine.java
@@ -3,6 +3,7 @@ package mc.core.engine;
 import java.util.Queue;
 
 import mc.core.engine.model.InputEvent;
+import mc.core.event.EventProvider;
 import mc.core.world.Player;
 import mc.core.world.PlayerView;
 import mc.core.world.event.ChunkEvent;
@@ -14,15 +15,18 @@ public interface Engine {
 	Queue<ChunkEvent> getChunkQueue();
 
 	Player getPlayer();
-	
+
 	PlayerView getView();
-	
+
 	void setPlayer(Player player);
-	
+
 	void setView(PlayerView view);
-	
+
 	void start();
-	
+
 	String getDescription();
 
+	EventProvider<WindowCloseEvent> OnClosingWindow();
+
+	void close();
 }
--- GameEngine/src/main/java/mc/engine/OpenGLCppEngine.java
+++ GameEngine/src/main/java/mc/engine/OpenGLCppEngine.java
@@ -9,8 +9,11 @@ import org.joml.Vector3d;
 import lombok.Getter;
 import lombok.Setter;
 import mc.core.engine.Engine;
+import mc.core.engine.WindowCloseEvent;
 import mc.core.engine.model.InputEvent;
+import mc.core.event.Event;
 import mc.core.event.EventListener;
+import mc.core.event.EventProvider;
 import mc.core.event.OldNewEvent;
 import mc.core.world.Player;
 import mc.core.world.PlayerView;
@@ -23,17 +26,18 @@ public class OpenGLCppEngine implements Engine {
 	private Queue<ChunkEvent> chunk;
 	private Queue<OldNewEvent<Vector3d>> position;
 	private Queue<OldNewEvent<Vector2d>> camera;
-	private PlayerPositionListener positionListener= new PlayerPositionListener();
-	private PlayerCameraListener cameraListener= new PlayerCameraListener();
+	private PlayerPositionListener positionListener = new PlayerPositionListener();
+	private PlayerCameraListener cameraListener = new PlayerCameraListener();
 	private Player player;
+	private Event<WindowCloseEvent> onClose;
 	@Setter
 	private PlayerView view;
-	
+
 	public void setPlayer(Player player) {
-		if(player == null) {
+		if (player == null) {
 			throw new IllegalArgumentException("cannot set a player which is null!");
 		}
-		if(this.player != null) {
+		if (this.player != null) {
 			this.player.OnPositionUpdate().remove(this.positionListener);
 			this.player.OnCameraUpdate().remove(this.cameraListener);
 		}
@@ -42,22 +46,23 @@ public class OpenGLCppEngine implements Engine {
 		this.player.OnCameraUpdate().add(this.cameraListener);
 	}
 
-	public Queue<OldNewEvent<Vector3d>> getPositionUpdate(){
-		if(this.position == null) {
+	public Queue<OldNewEvent<Vector3d>> getPositionUpdate() {
+		if (this.position == null) {
 			this.position = new LinkedList<>();
 		}
 		return this.position;
 	}
-	public Queue<OldNewEvent<Vector2d>> getCameraUpdate(){
-		if(this.camera == null) {
+
+	public Queue<OldNewEvent<Vector2d>> getCameraUpdate() {
+		if (this.camera == null) {
 			this.camera = new LinkedList<>();
 		}
 		return this.camera;
 	}
-	
+
 	@Override
 	public Queue<InputEvent> getInputQueue() {
-		if(this.input == null) {
+		if (this.input == null) {
 			this.input = new LinkedList<>();
 		}
 		return this.input;
@@ -65,7 +70,7 @@ public class OpenGLCppEngine implements Engine {
 
 	@Override
 	public Queue<ChunkEvent> getChunkQueue() {
-		if(this.chunk == null) {
+		if (this.chunk == null) {
 			this.chunk = new LinkedList<>();
 		}
 		return this.chunk;
@@ -73,37 +78,47 @@ public class OpenGLCppEngine implements Engine {
 
 	@Override
 	public void start() {
-		if(this.player == null) {
+		if (this.player == null) {
 			throw new IllegalArgumentException("Cannot start the Engine without a player");
 		}
-		if(this.view == null) {
+		if (this.view == null) {
 			throw new IllegalArgumentException("Cannot start the Engine without a view");
 		}
-		
-		//TODO add code which executes the cpp library
+
+		// TODO add code which executes the cpp library
 	}
 
 	@Override
 	public String getDescription() {
 		return "OpenGL 3 C++ Implementation";
 	}
-	
-	private class PlayerPositionListener implements EventListener<OldNewEvent<Vector3d>>{
+
+	@Override
+	public EventProvider<WindowCloseEvent> OnClosingWindow() {
+		return this.onClose;
+	}
+
+	@Override
+	public void close() {
+		this.onClose.invoke(this, new WindowCloseEvent(true));
+	}
+
+	private class PlayerPositionListener implements EventListener<OldNewEvent<Vector3d>> {
 
 		@Override
 		public void listen(Object sender, OldNewEvent<Vector3d> object) {
 			position.add(object);
 		}
-		
+
 	}
-	
-	private class PlayerCameraListener implements EventListener<OldNewEvent<Vector2d>>{
+
+	private class PlayerCameraListener implements EventListener<OldNewEvent<Vector2d>> {
 
 		@Override
 		public void listen(Object sender, OldNewEvent<Vector2d> object) {
 			camera.add(object);
 		}
-		
+
 	}
 
 }
