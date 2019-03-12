0d2c812d2ad88a9faa23cb8eec1238ec9a0b955a
BirdIT <nico-vogel97@outlook.de>
2019-03-12 21:42:48 +0100

    add

---
+++ GameCore/src/main/java/mc/core/engine/WindowCloseEvent.java
@@ -0,0 +1,12 @@
+package mc.core.engine;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class WindowCloseEvent {
+
+	private boolean userAction;
+
+}
