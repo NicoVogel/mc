72e8c332c2d569a67d087036ed7dd24d3e5b7242
BirdIT <nico-vogel97@outlook.de>
2019-03-23 15:03:47 +0100

    add chunk object

--- GameCore/src/main/java/mc/core/world/event/ChunkEvent.java
+++ GameCore/src/main/java/mc/core/world/event/ChunkEvent.java
@@ -1,6 +1,7 @@
 package mc.core.world.event;
 
 import lombok.Getter;
+import mc.core.world.Chunk;
 
 @Getter
 public class ChunkEvent extends PlayerViewEvent {
@@ -8,12 +9,20 @@ public class ChunkEvent extends PlayerViewEvent {
 	private int posX;
 	private int posY;
 	private ChunkEventType type;
+	private Chunk chunk;
 
-	public ChunkEvent(int posX, int posY, ChunkEventType type) {
+	public ChunkEvent(int posX, int posY, Chunk chunk) {
+		this(posX, posY, chunk, ChunkEventType.ADD);
+	}
+
+	public ChunkEvent(int posX, int posY) {
+		this(posX, posY, null, ChunkEventType.REMOVE);
+	}
+
+	private ChunkEvent(int posX, int posY, Chunk chunk, ChunkEventType type) {
 		super(true);
 		this.posX = posX;
 		this.posY = posY;
 		this.type = type;
 	}
-
 }
