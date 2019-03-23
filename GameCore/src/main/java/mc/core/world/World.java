f8beae10cbe1ebfac1d23d4b582b3d011325d5a4
BirdIT <nico-vogel97@outlook.de>
2019-03-23 14:29:49 +0100

    implement world generation call

--- GameCore/src/main/java/mc/core/world/World.java
+++ GameCore/src/main/java/mc/core/world/World.java
@@ -1,10 +1,12 @@
 package mc.core.world;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.joml.Vector2i;
 
 import lombok.Getter;
+import mc.core.world.generation.WorldGenerationManager;
 
 @Getter
 public class World {
@@ -14,12 +16,27 @@ public class World {
 	public final int chunkHeight;
 
 	public Map<Vector2i, Chunk> chunks;
-	
-	public World(int width, int length,int height) {
+
+	public World(int width, int length, int height) {
 		this.chunkWidth = width;
 		this.chunkLength = length;
 		this.chunkHeight = height;
 	}
 
+	private Map<Vector2i, Chunk> getChunks() {
+		if (this.chunks == null) {
+			this.chunks = new TreeMap<>();
+		}
+		return this.chunks;
+	}
+
+	public Chunk getChunk(int x, int y) {
+		return getChunk(new Vector2i(x, y));
+	}
+
+	public Chunk getChunk(Vector2i position) {
+		return getChunks().computeIfAbsent(position,
+				postion -> WorldGenerationManager.getInstance().generateChunk(this, position));
+	}
 
 }
