4419146feb16dab7dfb8ad8d09e5a0c6c2fb3a76
BirdIT <nico-vogel97@outlook.de>
2019-07-01 00:20:07 +0200

    add comments

--- GameCore/src/main/java/mc/core/Component.java
+++ GameCore/src/main/java/mc/core/Component.java
@@ -3,15 +3,29 @@ package mc.core;
 import mc.core.event.Disposable;
 
 /**
- * Component
+ * This interface allows for maximum flexibility while creating all components
  */
 public interface Component extends Disposable, ComponentOrganizer, ElementStatus {
 
+    /**
+     * provide the parent game object
+     */
     public GameObject getGameObject();
 
+    /**
+     * provide the parent component, is only set if this component is a children of
+     * another component
+     */
     public Component getParent();
 
+    /**
+     * get a tag which can be set by a programmer. The advantage is that components
+     * can be fund by the tag. multiple components can have the same tag
+     */
     public String getTag();
 
+    /**
+     * tag a component to identify it later on
+     */
     public void setTag(String tag);
 }
