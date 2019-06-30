2edc02872dd5de5bff623c25e822e9413ec5fe5c
BirdIT <nico-vogel97@outlook.de>
2019-07-01 00:06:27 +0200

    init

---
+++ GameCore/src/test/java/mc/core/ComponentOrganizerTest.java
@@ -0,0 +1,15 @@
+package mc.core;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * ComponentOrganizerTest
+ */
+public class ComponentOrganizerTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
