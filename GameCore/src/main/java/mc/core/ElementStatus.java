9705827369b60c4ede9bda8b64d7cf36e2402056
BirdIT <nico-vogel97@outlook.de>
2019-07-01 00:05:02 +0200

    add

---
+++ GameCore/src/main/java/mc/core/ElementStatus.java
@@ -0,0 +1,13 @@
+package mc.core;
+
+/**
+ * ElementStatus
+ */
+public interface ElementStatus {
+
+    public boolean isActive();
+
+    public void setActive(boolean active);
+
+    public boolean isClosed();
+}
