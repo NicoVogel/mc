6f85a7b8ace45bd14f79788fc8104e7317593cb8
Nico Vogel <nico-vogel97@outlook.de>
2019-07-04 13:24:28 +0200

    implement tests to find objects by tag

--- GameCore/src/test/java/mc/core/ComponentOrganizerTest.java
+++ GameCore/src/test/java/mc/core/ComponentOrganizerTest.java
@@ -3,7 +3,11 @@ package mc.core;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import one.util.streamex.StreamEx;
@@ -14,6 +18,7 @@ import one.util.streamex.StreamEx;
 public class ComponentOrganizerTest {
 
     @Test
+    @Order(0)
     public void createFindComponentByTag() {
         // Arrange
         GameObject gameObj = new GameObject();
@@ -22,16 +27,39 @@ public class ComponentOrganizerTest {
         Component component2 = new ComponentStandard(gameObj);
         String tag = "test";
         component.setTag(tag);
+        component1.setTag("some other tag");
+        component2.setTag("some other tag2");
 
         // Act
         Component result = gameObj.getComponent(tag);
-        StreamEx<Component> result2 = gameObj.getComponents(tag);
+        List<Component> result2 = gameObj.getComponents(tag).toList();
 
         // Assert
         assertNotNull(result, "The tagged object was not found");
-        assertEquals(component, result2.findFirst());
-        assertSame(component, result, "the found object is not the same as the created object");
+        assertSame(component, result, "the found object is not the same as the first object");
+        assertSame(component, result2.get(0), "the first component is not equals to the first added component");
+    }
 
+    @Test
+    @Order(1)
+    public void createFindMultipleComponentsBySameTag() {
+        // Arrange
+        GameObject gameObj = new GameObject();
+        Component component = new ComponentStandard(gameObj);
+        Component component1 = new ComponentStandard(gameObj);
+        Component component2 = new ComponentStandard(gameObj);
+        String tag = "test";
+        component.setTag(tag);
+        component1.setTag(tag);
+        component2.setTag("some other tag");
+
+        // Act
+        List<Component> result2 = gameObj.getComponents(tag).toList();
+
+        // Assert
+        assertEquals(2l, result2.size(), "didn't found both components");
+        assertTrue(result2.contains(component), "the component which was added first is not part of the result");
+        assertTrue(result2.contains(component1), "the component which was added second is not part of the result");
     }
 
 }
