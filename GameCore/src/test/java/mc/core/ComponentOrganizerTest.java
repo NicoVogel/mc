312203014a7a1c26ee77ddd1f61ce50eab7d71dc
Nico Vogel <nico-vogel97@outlook.de>
2019-07-04 13:27:59 +0200

    implement get by class tests

--- GameCore/src/test/java/mc/core/ComponentOrganizerTest.java
+++ GameCore/src/test/java/mc/core/ComponentOrganizerTest.java
@@ -10,8 +10,6 @@ import java.util.List;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
-import one.util.streamex.StreamEx;
-
 /**
  * ComponentOrganizerTest
  */
@@ -55,6 +53,40 @@ public class ComponentOrganizerTest {
 
         // Act
         List<Component> result2 = gameObj.getComponents(tag).toList();
+
+        // Assert
+        assertEquals(2l, result2.size(), "didn't found both components");
+        assertTrue(result2.contains(component), "the component which was added first is not part of the result");
+        assertTrue(result2.contains(component1), "the component which was added second is not part of the result");
+    }
+
+    @Test
+    @Order(0)
+    public void createFindComponentByClass() {
+        // Arrange
+        GameObject gameObj = new GameObject();
+        Component component = new ComponentStandard(gameObj);
+
+        // Act
+        ComponentStandard result = gameObj.getComponent(ComponentStandard.class);
+        List<ComponentStandard> result2 = gameObj.getComponents(ComponentStandard.class).toList();
+
+        // Assert
+        assertNotNull(result, "no component with of ComponentStandard class was found");
+        assertSame(component, result, "the found object is not the same as the first object");
+        assertSame(component, result2.get(0), "the first component is not equals to the first added component");
+    }
+
+    @Test
+    @Order(1)
+    public void createFindMultipleComponentsBySameClass() {
+        // Arrange
+        GameObject gameObj = new GameObject();
+        Component component = new ComponentStandard(gameObj);
+        Component component1 = new ComponentStandard(gameObj);
+
+        // Act
+        List<ComponentStandard> result2 = gameObj.getComponents(ComponentStandard.class).toList();
 
         // Assert
         assertEquals(2l, result2.size(), "didn't found both components");
